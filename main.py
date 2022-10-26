850f8bb158b0ea35c727b1e1912869c68bbfee7d
Joao <sil24edu@gmail.com>
2022-10-26 14:18:58 -0500

    Incluimos el objeto Scena

--- main.py
+++ main.py
@@ -20,9 +20,13 @@ class Mygame(arcade.Window):
         
         # Define first attributes
         # These are 'lists' that keep track of our sprites. Each sprite should
+        
         # go into a list.
-        self.wall_list = None
-        self.player_list = None
+        # self.wall_list = None
+        # self.player_list = None
+        
+        #Our scene object
+        self.scene = None
         
         # Separate variable that holds the player sprite
         self.player_sprite = None
@@ -33,22 +37,31 @@ class Mygame(arcade.Window):
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
 
+        # Initialize Scene
+        self.scene = arcade.Scene()
+        
+        # Create the Sprite lists
+        self.scene.add_sprite_list('Player')
+        self.scene.add_sprite_list('Walls', use_spatial_hash=True)        
+        
         # Create the sprite lists
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        # self.player_list = arcade.SpriteList()
+        # self.wall_list = arcade.SpriteList(use_spatial_hash=True)
 
         # Set up the player, specifically placing it at these coordinates
         image_player_source = './img/small_mario_right.png'
         self.player_sprite = arcade.Sprite(image_player_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 96
-        self.player_list.append(self.player_sprite)
+        self.scene.add_sprite('Player', self.player_sprite)
+        # self.player_list.append(self.player_sprite)
 
         # Create the ground
         wall = arcade.Sprite('./img/floor.png', TILE_SCALING)
         wall.center_x = 500
         wall.center_y = 64
-        self.wall_list.append(wall)
+        self.scene.add_sprite("Walls", wall)
+        # self.wall_list.append(wall)
 
         # Create boxes
         coordinate_list_box = [[512, 96], [256,96], [768, 96]]
@@ -57,7 +70,8 @@ class Mygame(arcade.Window):
             # Create boxes on the ground
             box = arcade.Sprite('./img/sbox.png',CHARACTER_SCALING)
             box.position = coordinate
-            self.wall_list.append(box)
+            self.scene.add_sprite('Wall', box)
+            # self.wall_list.append(box)
         
         # Create clouds
         coordinate_list_clouds = [[250, 300], [500, 300], [750, 300], [1000, 300]]
@@ -66,7 +80,8 @@ class Mygame(arcade.Window):
             # Create clouds on the sky
             cloud = arcade.Sprite('./img/clouds.png',CHARACTER_SCALING)
             cloud.position = coordinates
-            self.wall_list.append(cloud)
+            self.scene.add_sprite('Wall', cloud)
+            # self.wall_list.append(cloud)
         
     def on_draw(self):
         """Render the screen."""
@@ -76,9 +91,9 @@ class Mygame(arcade.Window):
         
         # Code to draw the screen goes here
         # Draw our sprites
-        self.wall_list.draw()
-        self.player_list.draw()
-
+        # self.wall_list.draw()
+        # self.player_list.draw()
+        self.scene.draw()
 
 def main():
     """ Main function """
